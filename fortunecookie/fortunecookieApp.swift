28ef1fe6db5b121bd9b04c1264c7b5fd43eb2c12
Dksie09 <dakshiegoel@gmail.com>
2024-05-22 21:14:57 +0530

    Initial Commit

---
+++ fortunecookie/fortunecookieApp.swift
@@ -0,0 +1,17 @@
+//
+//  fortunecookieApp.swift
+//  fortunecookie
+//
+//  Created by Dakshi Goel on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct fortunecookieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
